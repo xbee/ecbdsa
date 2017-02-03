568e5ded9bce458dc28b1375ec9eaff331ba271d
xbee <hfeeki@outlook.com>
2017-02-04 00:08:26 +0800

    remove all old files

--- ecbdsa.go
+++ ecbdsa.go
@@ -148,12 +148,13 @@ type Signer struct {
   d, k *big.Int
 
   // shareable stuff
-  Q, R *PublicKey
+  Q, R *ecdsa.PublicKey
 }
 
 func NewSigner(e elliptic.Curve, p, q *big.Int) (*Signer, error) {
   bob := new(Signer)
 
+  // generate signer's private & public key pair
   keys, err := GenerateKey(rand.Reader)
   maybePanic(err)
   bob.d = keys.D
@@ -164,44 +165,33 @@ func NewSigner(e elliptic.Curve, p, q *big.Int) (*Signer, error) {
 }
 
 
-func (bob *Signer) GetSessionKey() *PublicKey {
+func (bob *Signer) GenerateSessionKeyPair() *ecdsa.PublicKey {
   // generate k and R for each user request (§4.2)
-  if bob.k == nil {
-    request, err := GenerateKey(rand.Reader)
-    maybePanic(err)
-    bob.k = request.D
-    R := &request.PublicKey
-    return R
-  }
-  
-  R := ScalarBaseMult(bob.k)
-  return R 
+  request, err := GenerateKey(rand.Reader)
+  maybePanic(err)
+  bob.k = request.D
+  R := &request.PublicKey
+  return R
 }
 
 // Signs a blinded message
 // Bob signs the blinded hash and returns the signature to Alice: 
-// s1 = p·h2 + q (mod n).
-func (bob *Signer) Sign(h2 *big.Int) *big.Int {
-  c := bob.Curve
-  n := c.Params().N
-
-  s1 := new(big.Int).Mul(bob.p, h2)
-  s1.Add(s1, bob.q)
-  s1.Mod(s1, n)
-  return s1
+// s’ = dm’ + k.
+func (bob *Signer) BlindSign(mHat *big.Int) *big.Int {
+  crv := Secp256k1().Params()
 
   // verify that R matches our secret k
-  // R_ := ScalarBaseMult(sState.k)
-  // if !KeysEqual(R, R_) {
-  //   panic("unknown R")
-  // }
+  R_ := ScalarBaseMult(bob.k)
+  if !KeysEqual(bob.R, R_) {
+    panic("unknown R")
+  }
 
-  // // signer generates signature (§4.3)
-  // sHat := new(big.Int).Mul(sState.d, mHat)
-  // sHat.Add(sHat, sState.k)
-  // sHat.Mod(sHat, params.N)
+  // signer generates signature (§4.3)
+  sHat := new(big.Int).Mul(sState.d, mHat)
+  sHat.Add(sHat, sState.k)
+  sHat.Mod(sHat, crv.N)
 
-  // return sHat
+  return sHat
 }
 
 func BlindVerify(Q *ecdsa.PublicKey, msg []byte, sig *BlindSignature) bool {
@@ -210,11 +200,11 @@ func BlindVerify(Q *ecdsa.PublicKey, msg []byte, sig *BlindSignature) bool {
   M := hashToInt(msg, e)
 
   // onlooker verifies signature (§4.5)
-  sG := e.ScalarBaseMult(sig.S)
+  sG := ScalarBaseMult(sig.S)
   rm := new(big.Int).Mul(new(big.Int).Mod(sig.F.X, crv.N), M)
   rm.Mod(rm, crv.N)
-  rmQ := e.ScalarMult(rm, Q)
-  rmQplusF := e.Add(rmQ, sig.F)
+  rmQ := ScalarMult(rm, Q)
+  rmQplusF := Add(rmQ, sig.F)
 
   fmt.Println("")
   fmt.Printf("sG      = %x\n", sG.X)
@@ -307,128 +297,6 @@ func fermatInverse(k, N *big.Int) *big.Int {
 
 var errZeroParam = errors.New("zero parameter")
 
-// Sign signs a hash (which should be the result of hashing a larger message)
-// using the private key, priv. If the hash is longer than the bit-length of the
-// private key's curve order, the hash will be truncated to that length.  It
-// returns the signature as a pair of integers. The security of the private key
-// depends on the entropy of rand.
-func Sign(rand io.Reader, priv *PrivateKey, hash []byte) (r, s *big.Int, err error) {
-  // Get max(log2(q) / 2, 256) bits of entropy from rand.
-  entropylen := (priv.Curve.Params().BitSize + 7) / 16
-  if entropylen > 32 {
-    entropylen = 32
-  }
-  entropy := make([]byte, entropylen)
-  _, err = io.ReadFull(rand, entropy)
-  if err != nil {
-    return
-  }
-
-  // Initialize an SHA-512 hash context; digest ...
-  md := sha512.New()
-  md.Write(priv.D.Bytes()) // the private key,
-  md.Write(entropy)        // the entropy,
-  md.Write(hash)           // and the input hash;
-  key := md.Sum(nil)[:32]  // and compute ChopMD-256(SHA-512),
-  // which is an indifferentiable MAC.
-
-  // Create an AES-CTR instance to use as a CSPRNG.
-  block, err := aes.NewCipher(key)
-  if err != nil {
-    return nil, nil, err
-  }
-
-  // Create a CSPRNG that xors a stream of zeros with
-  // the output of the AES-CTR instance.
-  csprng := cipher.StreamReader{
-    R: zeroReader,
-    S: cipher.NewCTR(block, []byte(aesIV)),
-  }
-
-  // See [NSA] 3.4.1
-  c := priv.PublicKey.Curve
-  N := c.Params().N
-  if N.Sign() == 0 {
-    return nil, nil, errZeroParam
-  }
-  var k, kInv *big.Int
-  for {
-    for {
-      k, err = RandFieldElement(c, csprng)
-      if err != nil {
-        r = nil
-        return
-      }
-
-      if in, ok := priv.Curve.(invertible); ok {
-        kInv = in.Inverse(k)
-      } else {
-        kInv = fermatInverse(k, N) // N != 0
-      }
-
-      r, _ = priv.Curve.ScalarBaseMult(k.Bytes())
-      r.Mod(r, N)
-      if r.Sign() != 0 {
-        break
-      }
-    }
-
-    e := hashToInt(hash, c)
-    s = new(big.Int).Mul(priv.D, r)
-    s.Add(s, e)
-    s.Mul(s, kInv)
-    s.Mod(s, N) // N != 0
-    if s.Sign() != 0 {
-      break
-    }
-  }
-
-  return
-}
-
-// Verify verifies the signature in r, s of hash using the public key, pub. Its
-// return value records whether the signature is valid.
-func Verify(pub *PublicKey, hash []byte, r, s *big.Int) bool {
-  // See [NSA] 3.4.2
-  c := pub.Curve
-  N := c.Params().N
-
-  if r.Sign() <= 0 || s.Sign() <= 0 {
-    return false
-  }
-  if r.Cmp(N) >= 0 || s.Cmp(N) >= 0 {
-    return false
-  }
-  e := hashToInt(hash, c)
-
-  var w *big.Int
-  if in, ok := c.(invertible); ok {
-    w = in.Inverse(s)
-  } else {
-    w = new(big.Int).ModInverse(s, N)
-  }
-
-  u1 := e.Mul(e, w)
-  u1.Mod(u1, N)
-  u2 := w.Mul(r, w)
-  u2.Mod(u2, N)
-
-  // Check if implements S1*g + S2*p
-  var x, y *big.Int
-  if opt, ok := c.(combinedMult); ok {
-    x, y = opt.CombinedMult(pub.X, pub.Y, u1.Bytes(), u2.Bytes())
-  } else {
-    x1, y1 := c.ScalarBaseMult(u1.Bytes())
-    x2, y2 := c.ScalarMult(pub.X, pub.Y, u2.Bytes())
-    x, y = c.Add(x1, y1, x2, y2)
-  }
-
-  if x.Sign() == 0 && y.Sign() == 0 {
-    return false
-  }
-  x.Mod(x, N)
-  return x.Cmp(r) == 0
-}
 
 // Multiplies the base G by a large integer.  The resulting
 // point is represented as an ECDSA public key since that's
